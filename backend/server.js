9722dbc9e137c1374b144f788beab3d362a9e1f4
joana faria <paisdefaria.joana@gmail.com>
2025-01-22 17:58:40 +0000

    mongoDB setup and connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
+app.get('/products', (req, res) => {});
+
+
 app.listen(5000, () => {
+  connectDB();
   console.log('Server running on port 5000');
 });
