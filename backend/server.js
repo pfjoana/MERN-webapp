72db05334ee8b60f8da9c3b20a7de838376724cc
joana faria <paisdefaria.joana@gmail.com>
2025-01-23 18:55:18 +0000

    Create POST Route for Product Creation

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,39 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from'./models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => {});
+app.use(express.json()); //to accept JSON data in the req.body
 
+// app.get('/products', (req, res) => {});
+
+
+//create product
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  //check if all data is provided
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success:false, message: "Please fill in all the required fields"});
+  }
+
+  //create the product
+  const newProduct = new Product(product)
+
+  //save it in databse
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct});
+  } catch (error) {
+    console.error("Error in create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error"})
+  }
+
+});
 
 app.listen(5000, () => {
   connectDB();
