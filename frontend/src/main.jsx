9faeffa7b1b1e7779a4b5841a891c67544383a5b
joana faria <paisdefaria.joana@gmail.com>
2025-01-29 17:50:06 +0000

    Refactor project structure, update Chakra UI, and implement NavBar - Updated Chakra UI to latest version - Organized project with folders for components and pages - Set up routing in App.jsx and created respective files for Homepage and CreatePage - Added and styled NavBar with store name and 2 buttons (link to CreatePage and Switch Dark/Light Mode) - Implemented dark mode toggle using useColorMode from Chakra

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { Provider } from "@/components/ui/provider"
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import { ChakraProvider } from "@chakra-ui/react"
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider>
+      <ChakraProvider>
         <App />
-      </Provider>
+      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>,
 )
